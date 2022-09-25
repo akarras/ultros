4695db6fbba13edeae8f549b0012a30434c72b48
akarras <chw111111@hotmail.com>
2022-09-25 01:51:08 -0600

    use a world string instead of a random world id

--- ultros/src/discord/ffxiv/analyze.rs
+++ ultros/src/discord/ffxiv/analyze.rs
@@ -14,12 +14,14 @@ pub(crate) async fn analyze(ctx: Context<'_>) -> Result<(), Error> {
 #[poise::command(slash_command, prefix_command)]
 pub(crate) async fn profit(
     ctx: Context<'_>,
-    #[description = "World you want to try and sell items on"] world: i32,
+    #[description = "World you want to try and sell items on"] world: String,
     #[description = "Number of items required to be sold within the threshold"]
     number_recently_sold: i32,
     #[description = "Length of the threshold in days"] threshold_days: i32,
 ) -> Result<(), Error> {
     ctx.defer_ephemeral().await?;
+    let world = ctx.data().db.get_world(&world).await?;
+    let world = world.id;
     let threshold = number_recently_sold;
     let window = Duration::days(threshold_days.into());
     let xiv_data = xiv_gen_db::decompress_data();
