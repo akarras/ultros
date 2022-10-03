8ad3bcbe1994136b5f2a910cd21cb3d697f0221f
akarras <chw111111@hotmail.com>
2022-10-03 16:39:20 -0600

    time index migration. added 1gb with barely noticable speed increase

--- migration/src/m20221003_151617_add_time_index.rs
+++ migration/src/m20221003_151617_add_time_index.rs
@@ -8,13 +8,25 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-       
         manager
-            .create_index(IndexCreateStatement::new().name("sale_history_sold_date_index").table(SaleHistory::Table).col(SaleHistory::SoldDate).to_owned()).await
+            .create_index(
+                IndexCreateStatement::new()
+                    .name("sale_history_sold_date_index")
+                    .table(SaleHistory::Table)
+                    .col(SaleHistory::SoldDate)
+                    .to_owned(),
+            )
+            .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager.drop_index(IndexDropStatement::new().table(SaleHistory::Table).name("sale_history_sold_date_index").to_owned()).await
+        manager
+            .drop_index(
+                IndexDropStatement::new()
+                    .table(SaleHistory::Table)
+                    .name("sale_history_sold_date_index")
+                    .to_owned(),
+            )
+            .await
     }
 }
-
