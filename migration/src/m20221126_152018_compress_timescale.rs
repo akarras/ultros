0509c02642b39dc617971a2b38dbc2956558c698
akarras <chw111111@hotmail.com>
2022-12-27 16:21:50 -0700

    compression is annoying

--- migration/src/m20221126_152018_compress_timescale.rs
+++ migration/src/m20221126_152018_compress_timescale.rs
@@ -46,39 +46,39 @@ impl MigrationTrait for Migration {
                     .to_owned(),
             )
             .await?;
-        manager.exec_stmt(RawPostgresStatement{ statement: "ALTER TABLE sale_history SET (timescaledb.compress, timescaledb.compress_orderby = 'sold_date',
-        timescaledb.compress_segmentby = 'world_id, sold_item_id'
-        );".to_string()}).await?;
-        manager.exec_stmt(RawPostgresStatement{ statement: "CREATE MATERIALIZED VIEW sale_summary_daily (world_id, i_id, hq, time, min_price, median_price, max_price, start_price, end_price, number_sold)
-            WITH (timescaledb.continuous) AS
-            SELECT world_id, sold_item_id, hq, time_bucket('1day', sold_date), min(price_per_item) AS minp, percentile_disc(0.5) WITHIN GROUP (order by price_per_item) AS medianp, max(price_per_item) maxp, FIRST(price_per_item, sold_date) AS start_price, LAST(price_per_item, sold_date) AS end_price, COUNT(*) as number_sold
-            FROM sale_history
-            GROUP BY time_bucket('1day', sold_date), hq, sold_item_id, world_id".to_string()}).await?;
-        manager
-            .exec_stmt(RawPostgresStatement {
-                statement: "SELECT add_compression_policy('sale_history', INTERVAL '4 weeks');"
-                    .to_string(),
-            })
-            .await?;
+        // manager.exec_stmt(RawPostgresStatement{ statement: "ALTER TABLE sale_history SET (timescaledb.compress, timescaledb.compress_orderby = 'sold_date',
+        // timescaledb.compress_segmentby = 'world_id, sold_item_id'
+        // );".to_string()}).await?;
+        // manager.exec_stmt(RawPostgresStatement{ statement: "CREATE MATERIALIZED VIEW sale_summary_daily (world_id, i_id, hq, time, min_price, median_price, max_price, start_price, end_price, number_sold)
+        //     WITH (timescaledb.continuous) AS
+        //     SELECT world_id, sold_item_id, hq, time_bucket('1day', sold_date), min(price_per_item) AS minp, percentile_disc(0.5) WITHIN GROUP (order by price_per_item) AS medianp, max(price_per_item) maxp, FIRST(price_per_item, sold_date) AS start_price, LAST(price_per_item, sold_date) AS end_price, COUNT(*) as number_sold
+        //     FROM sale_history
+        //     GROUP BY time_bucket('1day', sold_date), hq, sold_item_id, world_id".to_string()}).await?;
+        // manager
+        //     .exec_stmt(RawPostgresStatement {
+        //         statement: "SELECT add_compression_policy('sale_history', INTERVAL '4 weeks');"
+        //             .to_string(),
+        //     })
+        //     .await?;
         Ok(())
         //
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .alter_table(
-                TableAlterStatement::new()
-                    .table(SaleHistory::Table)
-                    .add_column_if_not_exists(
-                        ColumnDef::new(SaleHistory::Id).auto_increment().integer(),
-                    )
-                    .to_owned(),
-            )
-            .await?;
+        // manager
+        //     .alter_table(
+        //         TableAlterStatement::new()
+        //             .table(SaleHistory::Table)
+        //             .add_column_if_not_exists(
+        //                 ColumnDef::new(SaleHistory::Id).auto_increment().integer(),
+        //             )
+        //             .to_owned(),
+        //     )
+        //     .await?;
 
-        manager.exec_stmt(RawPostgresStatement{ statement: "ALTER TABLE sale_history SET (timescaledb.compress = false, timescaledb.compress_orderby = '',
-        timescaledb.compress_segmentby = 'world_id, sold_item_id'
-        );".to_string()}).await?;
+        // manager.exec_stmt(RawPostgresStatement{ statement: "ALTER TABLE sale_history SET (timescaledb.compress = false, timescaledb.compress_orderby = '',
+        // timescaledb.compress_segmentby = 'world_id, sold_item_id'
+        // );".to_string()}).await?;
         Ok(())
     }
 }
