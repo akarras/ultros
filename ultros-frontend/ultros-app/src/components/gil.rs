1d76a2db320974542acb498f254c970d20c82975
akarras <chw111111@hotmail.com>
2023-01-04 20:30:18 -0700

    separate gil with commas

--- ultros-frontend/ultros-app/src/components/gil.rs
+++ ultros-frontend/ultros-app/src/components/gil.rs
@@ -1,9 +1,10 @@
 use leptos::*;
+use thousands::Separable;
 
 #[component]
 pub fn Gil(cx: Scope, amount: i32) -> impl IntoView {
     view! {
         cx,
-        <span class="gil"><img src="/static/images/gil.webp"/></span>{amount}
+        <span class="gil"><img src="/static/images/gil.webp"/></span>{amount.separate_with_commas()}
     }
 }
