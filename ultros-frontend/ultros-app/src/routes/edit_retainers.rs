663a84bcc9d2bbd6aeffd26d0d85e710be6b7667
akarras <chw111111@hotmail.com>
2023-01-21 13:58:16 -0700

    add retainers buttons

--- ultros-frontend/ultros-app/src/routes/edit_retainers.rs
+++ ultros-frontend/ultros-app/src/routes/edit_retainers.rs
@@ -2,7 +2,26 @@ use leptos::*;
 
 #[component]
 pub fn EditRetainers(cx: Scope) -> impl IntoView {
-    view! { cx, <div class="container">
+    view! { cx, 
+    <div class="container">
+      <div class="content-nav">
+        <a class="btn-secondary" href="/retainers/edit">
+          <i class="fa-solid fa-magnifying-glass"></i>
+          "Search Retainers"
+        </a>
+        <a class="btn-secondary active" href="/retainers/edit">
+            <span class="fa fa-pen-to-square"></span>
+            "Edit"
+        </a>
+        <a class="btn-secondary" href="/retainers/edit">
+            <span class="fa fa-pencil"></span>
+            "Listings"
+        </a>
+        <a class="btn-secondary" href="/retainers/edit">
+            <span class="fa fa-exclamation"></span>
+            "Undercuts"
+        </a>
+      </div>
       <div class="main-content">
 
       </div>
