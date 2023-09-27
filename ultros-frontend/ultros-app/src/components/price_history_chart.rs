a6743e6bf84f4dfbe864db8dd785f00dc0c0ef76
akarras <aaron@akarras.com>
2023-09-26 20:45:22 -0600

    fix: add auto margin and height constraint to pricing canvas. closes #32

--- ultros-frontend/ultros-app/src/components/price_history_chart.rs
+++ ultros-frontend/ultros-app/src/components/price_history_chart.rs
@@ -32,8 +32,8 @@ pub fn PriceHistoryChart(sales: MaybeSignal<Vec<SaleHistory>>) -> impl IntoView 
         }
     });
     view! {
-        <div class="flex flex-col" class:hidden=hidden>
-            <canvas width="750" height="450" _ref=canvas/>
+        <div class="flex flex-col max-h-[480px] mx-auto" class:hidden=hidden>
+            <canvas width="750" height="440" _ref=canvas/>
             <Toggle checked=filter_outliers set_checked=set_filter_outliers
                 checked_label="Filtering outliers" unchecked_label="No filter" />
         </div>
