dc63f6954f692f3ec421ad32a8b844979e929a26
akarras <aaron@akarras.com>
2023-09-29 18:49:11 -0600

    feat: reload ad on page nav

--- ultros-frontend/ultros-app/src/components/ad.rs
+++ ultros-frontend/ultros-app/src/components/ad.rs
@@ -7,7 +7,10 @@ pub fn Ad(#[prop(optional)] class: Option<&'static str>) -> impl IntoView {
     let ad_class = class.unwrap_or("h-64");
     let cookies = use_context::<Cookies>().unwrap();
     let (hide_ads, _) = cookies.use_cookie_typed::<_, bool>("HIDE_ADS");
+    let location = use_location();
+    let pathname = location.pathname;
     move || {
+        let _ = pathname(); // reading from the path to reload this component on page load
         (!hide_ads().unwrap_or_default()).then(move ||view!{
         <div class=ad_class>
             <div class="flex flex-col h-full">
