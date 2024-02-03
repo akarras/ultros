d17240f8973b9ca62696493397922ccfed38a260
akarras <chw111111@hotmail.com>
2024-02-03 16:04:44 -0700

    hide ads if they fail to load

--- ultros-frontend/ultros-app/src/components/ad.rs
+++ ultros-frontend/ultros-app/src/components/ad.rs
@@ -1,12 +1,37 @@
+use std::ops::Deref;
+
 use crate::Cookies;
-use leptos::*;
+use leptos::{html::Ins, *};
 use leptos_router::*;
+use leptos_use::{use_mutation_observer_with_options, UseMutationObserverOptions};
+use log::info;
 
 #[component]
 pub fn Ad(#[prop(optional)] class: Option<&'static str>) -> impl IntoView {
     let ad_class = class.unwrap_or("h-64");
+    let node = create_node_ref::<Ins>();
     let cookies = use_context::<Cookies>().unwrap();
     let (hide_ads, _) = cookies.use_cookie_typed::<_, bool>("HIDE_ADS");
+    let unfilled = create_rw_signal(false);
+    let _mutation_observer = use_mutation_observer_with_options(
+        node,
+        move |mutations, _| {
+            if let Some(_ad_fill_status) = mutations.into_iter().find(|record| {
+                record
+                    .attribute_name()
+                    .map(|name| name == "data-ad-status")
+                    .unwrap_or_default()
+            }) {
+                // just looking for data-ad-status="unfilled"
+                let node = node.get_untracked().unwrap();
+                if let Some(status) = node.deref().get_attribute("data-ad-status") {
+                    info!("ad status {status}");
+                    unfilled.set(status == "unfilled");
+                }
+            }
+        },
+        UseMutationObserverOptions::default().attributes(true),
+    );
     // let location = use_location();
     // let pathname = location.pathname;
     // let search = location.search;
@@ -14,19 +39,21 @@ pub fn Ad(#[prop(optional)] class: Option<&'static str>) -> impl IntoView {
         // let _ = pathname(); // reading from the path to reload this component on page load
         // let _ = search();
         (!hide_ads().unwrap_or_default()).then(move ||view!{
-        <div class=ad_class>
+        <div class:invisible=unfilled class=["ad ", ad_class].concat()>
             <div class="flex flex-col h-full">
                 <span class="text-lg">"Ad"</span>
                 <div class="flex-grow">
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8789160460804755"
-                        crossorigin="anonymous"></script>
+                        crossorigin="anonymous"
+                        on:error=move |_e| unfilled.set(true) ></script>
                     // <!-- Ultros-Ad-Main -->
                     <ins class="adsbygoogle"
                         style="display:block"
                         data-ad-client="ca-pub-8789160460804755"
                         data-ad-slot="1163555858"
                         data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
+                        data-full-width-responsive="true"
+                        node_ref=node></ins>
                     <script>
                         (adsbygoogle = window.adsbygoogle || []).push({});
                     </script>
