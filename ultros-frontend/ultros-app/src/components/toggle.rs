a9a86103a3e0ea9617e3ad66feeff2a442680bd2
akarras <aaron@akarras.com>
2024-03-29 17:32:24 -0600

    fix: dark mode toggle text shouldn't be dark

--- ultros-frontend/ultros-app/src/components/toggle.rs
+++ ultros-frontend/ultros-app/src/components/toggle.rs
@@ -14,8 +14,11 @@ pub fn Toggle(
                 let checked = checked.get_untracked();
                 set_checked(!checked);
             } />
-            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">{move || if checked() { checked_label.to_string() } else { unchecked_label.to_string() }}</span>
+            <div class="w-11 h-6 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-800 rounded-full peer bg-gray-700
+             peer-checked:after:translate-x-full peer-checked:after:border-white after:content-['']
+             after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border
+             after:rounded-full after:h-5 after:w-5 after:transition-all border-gray-600 peer-checked:bg-blue-600"></div>
+            <span class="ml-3 text-sm font-medium text-gray-300">{move || if checked() { checked_label.to_string() } else { unchecked_label.to_string() }}</span>
         </label>
 
     }
