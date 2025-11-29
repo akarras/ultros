bb9b8dd2340ec0ce1941cf9a10db5cdad44a25af
akarras <aaron@akarras.com>
2025-11-29 16:32:29 -0700

    fix clippy and warnings

--- ultros-db/src/lib.rs
+++ ultros-db/src/lib.rs
@@ -18,6 +18,7 @@ pub use sea_orm::error::DbErr as SeaDbErr;
 pub use sea_orm::ActiveValue;
 
 use anyhow::Result;
+use dotenvy::dotenv;
 use futures::future::try_join_all;
 use migration::{Migrator, MigratorTrait};
 
@@ -42,6 +43,7 @@ pub struct UltrosDb {
 impl UltrosDb {
     #[instrument]
     pub async fn connect() -> Result<Self> {
+        let _ = dotenv();
         let url = std::env::var("DATABASE_URL").expect("Missing DATABASE_URL environment variable");
         let mut opt = ConnectOptions::new(url);
         let max_connections = std::env::var("POSTGRES_MAX_CONNECTIONS")
